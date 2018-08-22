d79976c3ed31a64bed4e84a9d3bb48a09c8b9f2e
Ben Steadman <steadmanben1@gmail.com>
2018-08-22 08:07:08 +0100

    add error and exit traps to init.sh

--- tests/init.sh
+++ tests/init.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-# Use this function to prepend to PATH an absolute name for each
-# specified, possibly-$initial_cwd_-relative, directory.
-path_prepend_ ()
+set_fail()
 {
-  while test $# != 0; do
-    path_dir_=$1
-    PATH="$path_dir_:$PATH"
-    shift
-  done
-  export PATH
+  fail=1
 }
+
+finish()
+{
+  rm -f test.txt
+  fail=0
+}
+
 trap set_fail ERR
 
 trap finish EXIT
