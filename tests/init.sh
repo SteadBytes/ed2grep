982036497980dc30a83d3add07338b753ab60ed4
Ben Steadman <steadmanben1@gmail.com>
2018-08-29 07:47:05 +0100

    improve test output

--- tests/init.sh
+++ tests/init.sh
@@ -21,34 +21,35 @@ path_prepend_ ()
   export PATH
 }
 
-output_test_failure()
+output_match_test_failure()
 {
   input=$1
   pattern=$2
   expected=$3
-  name=$4
+  got=$4
+  name=$5
 
-  printf "Test \"%s\" failed\n\tinput: %s\n\tpattern: %s\n\texpected: %s\n" "$name" "$input" "$pattern" "$expected" >&2
+  printf "Match Test \"%s\" failed\n\tinput: %s\n\tpattern: %s\n\texpected: %s\n\tgot: %s\n" "$name" "$input" "$pattern" "$expected" "$got" >&2
 }
 
 match_test()
 {
   test_fail=0
-  input=$1
+  input=$(echo -e $1)
   pattern=$2
-  expected=$3
+  expected=$(echo -e $3)
   name=$4
 
   out=$(echo -e "$input" | grep "$pattern")
   if test "$out" != "$expected"; then
-    output_test_failure "$input" "$pattern" "$expected" "$name (stdin)"
+    output_match_test_failure "$input" "$pattern" "$expected" "$out" "$name (stdin)"
     test_fail=1
   fi
 
   echo -e "$input" > test.txt 
   out=$(grep "$pattern" test.txt)
   if test "$out" != "$expected"; then
-    output_test_failure "$input" "$pattern" "$expected" "${name} (file)"
+    output_match_test_failure "$input" "$pattern" "$expected" "$out" "${name} (file)"
     test_fail=1
   fi
 
@@ -59,24 +60,37 @@ match_test()
   return $test_fail
 }
 
+output_status_test_failure()
+{
+  input=$1
+  pattern=$2
+  expected=$3
+  got=$4
+  name=$5
+
+  printf "Status Test \"%s\" failed\n\tinput: %s\n\tpattern: %s\n\texpected status: %s\n\tgot: %s\n" "$name" "$input" "$pattern" "$expected" "$got" >&2
+}
+
 status_test()
 {
   test_fail=0
   input=$1
   pattern=$2
-  status=$3
+  expected_status=$3
   name=$4
 
   out=$(echo -e "$input" | grep "$pattern")
-  if test $? -ne $status; then
-    output_test_failure "$input" "$pattern" $status "$name (stdin)"
+  status=$?
+  if test $status -ne $expected_status; then
+    output_status_test_failure "$input" "$pattern" $expected_status $status "$name (stdin)"
     test_fail=1
   fi
 
   echo -e "$input" > test.txt 
   out=$(grep "$pattern" test.txt)
-  if test $? -ne $status; then
-    output_test_failure "$input" "$pattern" $status "${name} (file)"
+  status=$?
+  if test $status -ne $expected_status; then
+    output_status_test_failure "$input" "$pattern" $expected_status $status "${name} (file)"
     test_fail=1
   fi
 
